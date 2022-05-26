c30048367c246b60bb6121418e87873451e9a94e
ElainePereira <elainepereirasantosss@gmail.com>
2022-05-26 10:49:22 -0300

    organização da parte visual do projeto

--- codificador-decodificador.js
+++ codificador-decodificador.js
@@ -1,9 +1,9 @@
 var captar="";
 function codificador() {
   captar = document.getElementById("teste").value;
-  var rep1 = captar.replace(/a/gi, "ai");
-  var rep2 = rep1.replace(/e/gi, "enter");
-  var rep3 = rep2.replace(/i/gi, "imes");
+  var rep1 = captar.replace(/e/gi, "enter");
+  var rep2 = rep1.replace(/i/gi, "imes");
+  var rep3 = rep2.replace(/a/gi, "ai");
   var rep4 = rep3.replace(/o/gi, "ober");
   var rep5 = rep3.replace(/u/gi, "ufat");
 
@@ -12,9 +12,9 @@ function codificador() {
 
 function decodificador() {
 captar = document.getElementById("teste").value;
-  var rep1 = captar.replace(/ai/gi, "a");
-  var rep2 = rep1.replace(/enter/gi, "e");
-  var rep3 = rep2.replace(/imes/gi, "i");
+  var rep1 = captar.replace(/enter/gi, "e");
+  var rep2 = rep1.replace(/imes/gi, "i");
+  var rep3 = rep2.replace(/ai/gi, "a");
   var rep4 = rep3.replace(/ober/gi, "o");
   var rep5 = rep4.replace(/ufat/gi, "u");
 
